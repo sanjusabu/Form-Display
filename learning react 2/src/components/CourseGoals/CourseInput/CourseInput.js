b5e739ca44cccb3436cfb872ee71ae6b7a1cfcf1
sanjusabu <76938125+sanjusabu@users.noreply.github.com>
2022-08-31 23:08:53 +0530

    added form display project

---
+++ learning react 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+ 
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0)
+    {
+      setvalid(true)
+    }
+    setEnteredValue(event.target.value);
+  };
+  const [enteredvalid,setvalid]=useState(true)
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if(enteredValue.trim().length===0)
+    {
+      setvalid(false)
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!enteredvalid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} style={{borderColor: enteredvalid ? 'black' : 'red'}}/>
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
